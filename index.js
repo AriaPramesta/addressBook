a483dca3cdd19ac37bdeb780a59660b0a51541b1
AriaPramesta <pramestaaria@gmail.com>
2024-03-22 14:35:54 +0700

    Update js file

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const contacts = [
   {
     id: 1,
-    fullName: "someone",
-    email: "someone@gmail.com",
-    phone: "123-456-789-012",
+    fullName: "Aria Adi Pramesta",
+    email: "pramesta@gmail.com",
+    phone: "62893145367908",
   },
   {
     id: 2,
-    fullName: "sometwo",
-    email: "sometwo@gmail.com",
-    phone: "098-765-432-109",
+    fullName: "Oliver Sykes",
+    email: "olisykes@gmail.com",
+    phone: "628123476801",
   },
   {
     id: 3,
-    fullName: "somethree",
-    email: "somethree@gmail.com",
-    phone: "098-345-432-109",
+    fullName: "Elon Musk",
+    email: "elon@gmail.com",
+    phone: "6298345432109",
   },
 ];
 
-console.log(contacts);
+function renderContacts() {
+  for (let index = 0; index < contacts.length; index++) {
+    const contact = contacts[index];
+
+    console.log(
+      `${contact.id}. ${contact.fullName} ${contact.email} (${contact.phone})`
+    );
+  }
+}
+
+function addContacts(fullName, email, phone) {
+  const lastId = contacts[contacts.length - 1].id;
+
+  contacts.push({ id: lastId + 1, fullName, email, phone });
+
+  renderContacts();
+}
+
+renderContacts();
+addContacts("Jeff Bezos", "jef@gmail.com", "628908428935");
+addContacts("Mark Zuckerberg", "mark@gmail.com", "6281344987601");
